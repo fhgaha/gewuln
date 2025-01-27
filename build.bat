8c65113142e9e2bb6120273cc19e5d9ce06c9887
fhgaha <antsy1992@gmail.com>
2025-01-27 12:16:56 +0300

    switched to x64

--- build.bat
+++ build.bat
@@ -2,9 +2,12 @@
 
 pushd build
 
+call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
+
 cl -Zi ../src/win32_gewuln.cpp ^
-/I"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN32\include" ^
-/DGLFW_STATIC /link /LIBPATH:"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN32\lib-static-ucrt" ^
-glfw3dll.lib OpenGL32.lib 
+/I"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\include" ^
+/DGLFW_STATIC /link /LIBPATH:"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\lib-vc2019" ^
+glfw3dll.lib ^
+/LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.16299.0\um\x64" OpenGL32.lib
 
 popd

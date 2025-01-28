e23eb4c71c559800a58b48d63284062f63d777ac
fhgaha <antsy1992@gmail.com>
2025-01-28 16:31:45 +0300

    triangle

--- build.bat
+++ build.bat
@@ -4,10 +4,10 @@ pushd build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-cl -Zi ../src/win32_gewuln.cpp ^
+cl -Zi ../src/win32_gewuln.cpp ..\src\glad.c ^
 /I"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\include" ^
-/DGLFW_STATIC /link /LIBPATH:"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\lib-vc2019" ^
-glfw3dll.lib ^
+/I"D:\MyProjects\cpp\third_party\glad\include" ^
+/DGLFW_STATIC /link /LIBPATH:"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\lib-vc2019" glfw3dll.lib ^
 /LIBPATH:"C:\Program Files (x86)\Windows Kits\10\Lib\10.0.16299.0\um\x64" OpenGL32.lib
 
 popd

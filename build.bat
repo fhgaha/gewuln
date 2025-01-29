aad3dd5f32ddf87f5e2e91cf397bf5e00f241c5f
fhgaha <antsy1992@gmail.com>
2025-01-29 17:53:14 +0300

    quad using EBO

--- build.bat
+++ build.bat
@@ -4,7 +4,7 @@ pushd build
 
 call "C:\Program Files (x86)\Microsoft Visual Studio\2019\Community\VC\Auxiliary\Build\vcvars64.bat"
 
-cl -Zi ../src/win32_gewuln.cpp ..\src\glad.c ^
+cl -Zi /EHsc ../src/win32_gewuln.cpp ..\src\glad.c ^
 /I"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\include" ^
 /I"D:\MyProjects\cpp\third_party\glad\include" ^
 /DGLFW_STATIC /link /LIBPATH:"D:\MyProjects\cpp\third_party\glfw-3.4.bin.WIN64\lib-vc2019" glfw3dll.lib ^
--- src/win32_gewuln.cpp
+++ src/win32_gewuln.cpp
@@ -6,17 +6,22 @@
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
 
+void single_triangle(GLFWwindow * window, unsigned int shaderProgram);
+void two_triangles_EBO(GLFWwindow * window, unsigned int shaderProgram);
+
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
 
-const char *vertexShaderSource = "#version 330 core\n"
+const char *vertexShaderSource = 
+    "#version 330 core\n"
     "layout (location = 0) in vec3 aPos;\n"
     "void main()\n"
     "{\n"
     "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
     "}\0";
-const char *fragmentShaderSource = "#version 330 core\n"
+const char *fragmentShaderSource = 
+    "#version 330 core\n"
     "out vec4 FragColor;\n"
     "void main()\n"
     "{\n"
@@ -31,10 +36,6 @@ int main()
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
-
-#ifdef __APPLE__
-    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
-#endif
 
     // glfw window creation
     // --------------------
@@ -97,6 +98,19 @@ int main()
     glDeleteShader(vertexShader);
     glDeleteShader(fragmentShader);
 
+    // single_triangle(window, shaderProgram);
+    two_triangles_EBO(window, shaderProgram);
+
+    glDeleteProgram(shaderProgram);
+
+    // glfw: terminate, clearing all previously allocated GLFW resources.
+    // ------------------------------------------------------------------
+    glfwTerminate();
+    return 0;
+}
+
+void single_triangle(GLFWwindow * window, unsigned int shaderProgram)
+{
     // set up vertex data (and buffer(s)) and configure vertex attributes
     // ------------------------------------------------------------------
     float vertices[] = {
@@ -157,12 +171,103 @@ int main()
     // ------------------------------------------------------------------------
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-    glDeleteProgram(shaderProgram);
+}
 
-    // glfw: terminate, clearing all previously allocated GLFW resources.
-    // ------------------------------------------------------------------
-    glfwTerminate();
-    return 0;
+void two_triangles_EBO(GLFWwindow * window, unsigned int shaderProgram)
+{
+    /*
+       1         2
+        *       *
+
+            +
+
+        *       *
+       0         3
+    */
+
+    float vertices[] = {
+        -0.5f, -0.5f, 0.0f,  // left bot
+        -0.5f,  0.5f, 0.0f,  // left top 
+         0.5f,  0.5f, 0.0f,  // right top 
+         0.5f, -0.5f, 0.0f,  // right bot
+    }; 
+    unsigned int indeces[] = {
+        0, 1, 2,
+        2, 3, 0,
+    };
+
+    unsigned int VBO, VAO, EBO;
+
+    {   // generation
+        glGenVertexArrays(1, &VAO);
+        glGenBuffers(1, &VBO);
+        glGenBuffers(1, &EBO);
+    }
+
+    {   // VAO bindings
+        glBindVertexArray(VAO);
+        
+        {   // VBO bindings
+            glBindBuffer(GL_ARRAY_BUFFER, VBO);
+            glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
+            
+            // VBO specific stuff
+            glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
+            glEnableVertexAttribArray(0);
+        }
+
+        {   // EBO bindings
+            glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
+            glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indeces), indeces, GL_STATIC_DRAW);
+        }
+
+    }
+
+    // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
+    glBindBuffer(GL_ARRAY_BUFFER, 0); 
+
+    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
+    // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
+    glBindVertexArray(0); 
+
+
+    // uncomment this call to draw in wireframe polygons.
+    glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+
+    // render loop
+    // -----------
+    while (!glfwWindowShouldClose(window))
+    {
+        // input
+        // -----
+        processInput(window);
+
+        // render
+        // ------
+        glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        // draw our first triangle
+        glUseProgram(shaderProgram);
+        glBindVertexArray(VAO); // seeing as we only have a single VAO there's no need to bind it every time, but we'll do so to keep things a bit more organized
+        
+        { //either of two:
+            glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);    //EBO
+            // glDrawArrays(GL_TRIANGLES, 0, 3);    // VBO
+        }
+        
+        // glBindVertexArray(0); // no need to unbind it every time 
+ 
+        // glfw: swap buffers and poll IO events (keys pressed/released, mouse moved etc.)
+        // -------------------------------------------------------------------------------
+        glfwSwapBuffers(window);
+        glfwPollEvents();
+    }
+
+    // optional: de-allocate all resources once they've outlived their purpose:
+    // ------------------------------------------------------------------------
+    glDeleteVertexArrays(1, &VAO);
+    glDeleteBuffers(1, &VBO);
 }
 
 // process all input: query GLFW whether relevant keys are pressed/released this frame and react accordingly

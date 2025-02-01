ac224f95be01a41dcbf7be8e98c7a363a2a6f391
fhgaha <antsy1992@gmail.com>
2025-02-01 04:35:56 +0300

    Shader class

--- src/win64_gewuln.cpp
+++ src/win64_gewuln.cpp
@@ -2,11 +2,7 @@
 #include <GLFW/glfw3.h>
 
 #include <iostream>
-#include <fstream>
-#include <sstream>
-#include <string>
-#include <filesystem>
-#include <string_view>
+#include <gewuln/shader.h>
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void processInput(GLFWwindow *window);
@@ -17,42 +13,6 @@ GLenum glCheckError_(const char *file, int line);
 // settings
 const unsigned int SCR_WIDTH = 800;
 const unsigned int SCR_HEIGHT = 600;
-
-const char *vertexShaderSource = 
-    "#version 330 core\n"
-    "layout (location = 0) in vec3 aPos;\n"
-    "out vec4 vertex_color;\n"
-    "void main()\n"
-    "{\n"
-    "   gl_Position = vec4(aPos.x, aPos.y, aPos.z, 1.0);\n"
-    "   vertex_color = vec4(0.5, 0.0, 0.0, 1.0);\n"
-    "}\0";
-const char *fragmentShaderSource = 
-    "#version 330 core\n"
-    "in vec4 vertex_color;"
-    "out vec4 FragColor;\n"
-    "uniform vec4 outColor;\n" //removed by compiler if not used
-    "void main()\n"
-    "{\n"
-    // "   FragColor = vec4(1.0f, 0.5f, 0.2f, 1.0f);\n"
-    // "   FragColor = vertex_color;\n"
-    "   FragColor = ourColor;\n"
-    "}\n\0";
-
-std::string readFile(const std::filesystem::path& filePath) {
-    if (!std::filesystem::exists(filePath)) {
-        throw std::runtime_error("File not found: " + filePath.string());
-    }
-
-    std::ifstream file(filePath);
-    if (!file.is_open()) {
-        throw std::runtime_error("Failed to open file: " + filePath.string());
-    }
-
-    std::stringstream buffer;
-    buffer << file.rdbuf();
-    return buffer.str();
-}
 
 int main()
 {
@@ -85,58 +45,13 @@ int main()
         return -1;
     }
 
-    std::string vertexShaderSourceStr = readFile("src/shaders/vertex.vert");
-    std::string fragmentShaderSourceStr = readFile("src/shaders/fragment.frag");
-
-    const char* vertexShaderSource = vertexShaderSourceStr.c_str();
-    const char* fragmentShaderSource = fragmentShaderSourceStr.data();
-
-    // build and compile our shader program
-    // ------------------------------------
-    // vertex shader
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, NULL);
-    glCompileShader(vertexShader);
-    // check for shader compile errors
-    int success;
-    char infoLog[512];
-    glGetShaderiv(vertexShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(vertexShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::VERTEX::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // fragment shader
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, NULL);
-    glCompileShader(fragmentShader);
-    // check for shader compile errors
-    glGetShaderiv(fragmentShader, GL_COMPILE_STATUS, &success);
-    if (!success)
-    {
-        glGetShaderInfoLog(fragmentShader, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::FRAGMENT::COMPILATION_FAILED\n" << infoLog << std::endl;
-    }
-    // link shaders
-    unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
-    glLinkProgram(shaderProgram);
-    // check for linking errors
-    glGetProgramiv(shaderProgram, GL_LINK_STATUS, &success);
-    if (!success) {
-        glGetProgramInfoLog(shaderProgram, 512, NULL, infoLog);
-        std::cout << "ERROR::SHADER::PROGRAM::LINKING_FAILED\n" << infoLog << std::endl;
-    }
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
-
+    Shader ourShader("src/shaders/vertex.vert", "src/shaders/fragment.frag");
 
     // set up vertex data (and buffer(s)) and configure vertex attributes
     float vertices[] = {
          0.5f, -0.5f, 0.0f, 1.0f, 0.0f, 0.0f, // right 
         -0.5f, -0.5f, 0.0f, 0.0f, 1.0f, 0.0f, // left  
-         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f// top   
+         0.0f,  0.5f, 0.0f, 0.0f, 0.0f, 1.0f  // top   
     }; 
 
     unsigned int VBO, VAO;
@@ -161,10 +76,8 @@ int main()
     // note that this is allowed, the call to glVertexAttribPointer registered VBO as the vertex attribute's bound vertex buffer object so afterwards we can safely unbind
     glBindBuffer(GL_ARRAY_BUFFER, 0); 
 
-    // You can unbind the VAO afterwards so other VAO calls won't accidentally modify this VAO, but this rarely happens. Modifying other
     // VAOs requires a call to glBindVertexArray anyways so we generally don't unbind VAOs (nor VBOs) when it's not directly necessary.
     glBindVertexArray(0); 
-
 
     // uncomment this call to draw in wireframe polygons.
     //glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
@@ -180,15 +93,8 @@ int main()
         glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
         glClear(GL_COLOR_BUFFER_BIT);
 
-    
-        // draw 
-        glUseProgram(shaderProgram);
-        
-        // use uniform to change color cyclicly
-        float timeValue = glfwGetTime();
-        float greenValue = sin(timeValue)/2.0f + 0.5f;  // to change in 0 - 1 range
-        int vertexColorLocation = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(vertexColorLocation, 0.0f, greenValue, 0.0f, 1.0f);
+        ourShader.use();
+        // ourShader.setFloat("someUniform", 1.0f);
 
         glBindVertexArray(VAO);
         glDrawArrays(GL_TRIANGLES, 0, 3);
@@ -201,8 +107,6 @@ int main()
     // optional: de-allocate all resources once they've outlived their purpose:
     glDeleteVertexArrays(1, &VAO);
     glDeleteBuffers(1, &VBO);
-
-    glDeleteProgram(shaderProgram);
 
     // glfw: terminate, clearing all previously allocated GLFW resources.
     glfwTerminate();

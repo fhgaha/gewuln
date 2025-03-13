ce83ae98bed2aa59e7893e55a7e8aca51af6ee13
fhgaha <antsy1992@gmail.com>
2025-03-13 12:32:58 +0300

    forcing exe app to run on gpu 1 which is descrete gpu in my case

--- src/win64_gewuln.cpp
+++ src/win64_gewuln.cpp
@@ -9,6 +9,10 @@
 #include <iostream>
 #include "game.h"
 
+// Use NVIDIA Gpu for NVIDIA Optimus laptops/GPUs
+extern "C" {
+    __declspec(dllexport) unsigned long NvOptimusEnablement = 0x00000001;
+}
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);

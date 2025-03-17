48b590140fa8eb1706c7efbdf9a0e5cb5d13f15f
fhgaha <antsy1992@gmail.com>
2025-03-18 00:42:50 +0300

    managed to fix mona's hand with new animation set

--- src/win64_gewuln.cpp
+++ src/win64_gewuln.cpp
@@ -18,6 +18,11 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
 void key_callback(GLFWwindow* window, int key, int scancode, int action, int mode);
+void APIENTRY glDebugOutput(
+    GLenum source, GLenum type, unsigned int id,
+    GLenum severity, GLsizei length,
+    const char *message, const void *userParam
+);
 
 GLenum glCheckError_(const char *file, int line);
 #define glCheckError() glCheckError_(__FILE__, __LINE__)
@@ -83,6 +88,15 @@ int main()
         if (flags & GL_CONTEXT_FLAG_DEBUG_BIT)
         {
             std::cout << "Successfully initialized Open GL debug context\n";
+            glEnable(GL_DEBUG_OUTPUT);
+            glEnable(GL_DEBUG_OUTPUT_SYNCHRONOUS);
+            glDebugMessageCallback(glDebugOutput, nullptr);
+            glDebugMessageControl(
+                  GL_DEBUG_SOURCE_API
+                , GL_DEBUG_TYPE_ERROR
+                , GL_DEBUG_SEVERITY_HIGH
+                , 0, nullptr, GL_TRUE
+            );
         }
     }    
     
@@ -190,20 +204,66 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
 GLenum glCheckError_(const char *file, int line)
 {
     GLenum errorCode;
-    while ((errorCode = glGetError()) != GL_NO_ERROR)
-    {
-        std::string error;
-        switch (errorCode)
-        {
-            case GL_INVALID_ENUM:         error = "INVALID_ENUM"; break;
-            case GL_INVALID_VALUE:        error = "INVALID_VALUE"; break;
-            case GL_INVALID_OPERATION:    error = "INVALID_OPERATION"; break;
-            // case GL_STACK_OVERFLOW:    error = "STACK_OVERFLOW"; break;  //not working
-            // case GL_STACK_UNDERFLOW:   error = "STACK_UNDERFLOW"; break; //not working
-            case GL_OUT_OF_MEMORY:        error = "OUT_OF_MEMORY"; break;
-            case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
-        }
-        std::cout << "--!!--" << error << " | " << file << " (" << line << ")" << std::endl;
-    }
+    // while ((errorCode = glGetError()) != GL_NO_ERROR)
+    // {
+    //     std::string error;
+    //     switch (errorCode)
+    //     {
+    //         case GL_INVALID_ENUM:         error = "INVALID_ENUM"; break;
+    //         case GL_INVALID_VALUE:        error = "INVALID_VALUE"; break;
+    //         case GL_INVALID_OPERATION:    error = "INVALID_OPERATION"; break;
+    //         // case GL_STACK_OVERFLOW:    error = "STACK_OVERFLOW"; break;  //not working
+    //         // case GL_STACK_UNDERFLOW:   error = "STACK_UNDERFLOW"; break; //not working
+    //         case GL_OUT_OF_MEMORY:        error = "OUT_OF_MEMORY"; break;
+    //         case GL_INVALID_FRAMEBUFFER_OPERATION: error = "INVALID_FRAMEBUFFER_OPERATION"; break;
+    //     }
+    //     std::cout << "--!!--" << error << " | " << file << " (" << line << ")" << std::endl;
+    // }
     return errorCode;
+}
+
+void APIENTRY glDebugOutput(
+    GLenum source,
+    GLenum type,
+    unsigned int id,
+    GLenum severity,
+    GLsizei length,
+    const char *message,
+    const void *userParam
+){
+    // ignore non-significant error/warning codes
+    if(id == 131169 || id == 131185 || id == 131218 || id == 131204) return;
+    std::cout << "---------------" << std::endl;
+    std::cout << "Debug message (" << id << "): " << message << std::endl;
+    switch (source)
+    {
+        case GL_DEBUG_SOURCE_API:               std::cout << "Source: API"; break;
+        case GL_DEBUG_SOURCE_WINDOW_SYSTEM:     std::cout << "Source: Window System"; break;
+        case GL_DEBUG_SOURCE_SHADER_COMPILER:   std::cout << "Source: Shader Compiler"; break;
+        case GL_DEBUG_SOURCE_THIRD_PARTY:       std::cout << "Source: Third Party"; break;
+        case GL_DEBUG_SOURCE_APPLICATION:       std::cout << "Source: Application"; break;
+        case GL_DEBUG_SOURCE_OTHER:             std::cout << "Source: Other"; break;
+    } std::cout << std::endl;
+    
+    switch (type)
+    {
+        case GL_DEBUG_TYPE_ERROR:               std::cout << "Type: Error"; break;
+        case GL_DEBUG_TYPE_DEPRECATED_BEHAVIOR: std::cout << "Type: Deprecated Behaviour"; break;
+        case GL_DEBUG_TYPE_UNDEFINED_BEHAVIOR:  std::cout << "Type: Undefined Behaviour"; break;
+        case GL_DEBUG_TYPE_PORTABILITY:         std::cout << "Type: Portability"; break;
+        case GL_DEBUG_TYPE_PERFORMANCE:         std::cout << "Type: Performance"; break;
+        case GL_DEBUG_TYPE_MARKER:              std::cout << "Type: Marker"; break;
+        case GL_DEBUG_TYPE_PUSH_GROUP:          std::cout << "Type: Push Group"; break;
+        case GL_DEBUG_TYPE_POP_GROUP:           std::cout << "Type: Pop Group"; break;
+        case GL_DEBUG_TYPE_OTHER:               std::cout << "Type: Other"; break;
+    } std::cout << std::endl;
+    
+    switch (severity)
+    {
+        case GL_DEBUG_SEVERITY_HIGH:            std::cout << "Severity: high"; break;
+        case GL_DEBUG_SEVERITY_MEDIUM:          std::cout << "Severity: medium"; break;
+        case GL_DEBUG_SEVERITY_LOW:             std::cout << "Severity: low"; break;
+        case GL_DEBUG_SEVERITY_NOTIFICATION:    std::cout << "Severity: notification"; break;
+    } std::cout << std::endl;
+    std::cout << std::endl;
 }

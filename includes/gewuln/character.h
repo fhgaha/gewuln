7093eb76373bc35da11f3e20eda461810d3e5868
fhgaha <antsy1992@gmail.com>
2025-05-02 13:44:54 +0300

    walkable area works

--- includes/gewuln/character.h
+++ includes/gewuln/character.h
@@ -39,28 +39,28 @@ class Character {
 		void ProcessInput(const bool keys[], Game *game, const float dt) {
 
 			if (keys[GLFW_KEY_E]){
-				
+
 				assert(this->model->collider_mesh.has_value() && "Character must have collider mesh!");
-				
+
 				//TODO should iterate through not just all loaded models but only through all the currently instanced models
 				std::map<std::string, Model> *models = &ResourceManager::Models;
 				for (const auto &[name, mdl] : *models)
 				{
 					// check if this character is colliding with an interactable
 					if (mdl.interactable_mesh.has_value()) {
-						
+
 						const std::vector<Vertex> &interactable_verts = mdl.interactable_mesh.value().vertices;
 
 						std::vector<Vertex> transformed_verts = this->model->collider_mesh.value().vertices;
 						for (size_t i = 0; i < transformed_verts.size(); i++){
 							transformed_verts[i].Position += this->position;
 						}
-						
+
 						bool collider_intersects_an_interactable = Geometry3d::intersect(
 							transformed_verts,
 							interactable_verts
 						);
-						
+
 						// if (collider_intersects_an_interactable){
 						// 	std::cout << "it intersects!\n";
 						// 	game->PlayCameraThing();
@@ -71,7 +71,7 @@ class Character {
 						return;
 					}
 				}
-				
+
 
 			}
 
@@ -92,103 +92,105 @@ class Character {
 				rot_rad += 2.0f * glm::pi<float>();
 			}
 
+			bool inside = false;
 			if (keys[GLFW_KEY_W]){
 			    velocity = forward * WALK_SPEED * dt;
-				
-				{// check if this character is inside of walkable area
-				
-					std::map<std::string, Model> *models = &ResourceManager::Models;
-					for (const auto &[name, mdl] : *models)
-					{
-						if (mdl.walkable_area.has_value()) {
-							
-							//1. get characters 4 lowest pts of collider
-							
-							std::array<glm::vec2, 4> small_square;
-							
-							//there are 4*6 verts (4 per side, 6 sides of a cube)
-							auto &verts = this->model->collider_mesh.value().vertices;
-							for (size_t i = 0; i < verts.size(); i+=4)
-							{
-								Vertex copy = verts[i];
-								
-								// get the edge with normal = (0, -1, 0)
-								//a == b when `std::fabsf(a - b) < 1e-6`
-								bool copy_normal_is_minus_one = std::fabsf(copy.Normal.y - (-1.0f)) < 1e-6;
-								if (copy_normal_is_minus_one){
-									glm::vec3 p0 = verts[i+0].Position + this->position;
-									glm::vec3 p1 = verts[i+1].Position + this->position;
-									glm::vec3 p2 = verts[i+2].Position + this->position;
-									glm::vec3 p3 = verts[i+3].Position + this->position;
-									
-									small_square = {
-										glm::vec2(p0.x, p0.z),
-										glm::vec2(p1.x, p1.z),
-										glm::vec2(p2.x, p2.z),
-										glm::vec2(p3.x, p3.z),	
-									};
-									
-									break;
-								}
-							}
-							
-							
-							//2. get all walkable areas triangles and their points
-							
-							std::vector<std::array<glm::vec2, 3>> walkable_area_tris;	
-													
-							const std::vector<Vertex> 		&walkable_verts 	= mdl.walkable_area.value().vertices;
-							const std::vector<unsigned int> &walkable_indeces 	= mdl.walkable_area.value().indices;
-							
-							//0 1 2, 0 2 3
-							for (size_t i = 0; i < walkable_indeces.size(); i+=3)
-							{
-								glm::vec3 p0 = walkable_verts[walkable_indeces[i+0]].Position;
-								glm::vec3 p1 = walkable_verts[walkable_indeces[i+1]].Position;
-								glm::vec3 p2 = walkable_verts[walkable_indeces[i+2]].Position;
-								
-								std::array<glm::vec2, 3> tri = {glm::vec2(p0.x, p0.z), glm::vec2(p1.x, p1.z), glm::vec2(p2.x, p2.z)};
-								walkable_area_tris.push_back(tri);
-							}
-							
 
-							//3. check the intersection with walkable area
+				std::map<std::string, Model> *models = &ResourceManager::Models;
+				for (const auto &[name, mdl] : *models) {
+					if (mdl.walkable_area.has_value()) {
 
-							bool inside = Geometry2d::rect_inside_area_of_tris(
-								small_square,
-								walkable_area_tris
-							);
-							
-							// std::cout << "small poly: \n";
-							// for (auto &pt_d : small_square)
-							// {
-							// 	std::cout << "\t{" << pt_d << "},";
-							// }
-							// std::cout << "\n";
-							
-							// std::cout << "walkable_pts:\n";
-							//...
-							std::cout << "\ninside area: " << inside << "\n"; 
-							// std::cout << "========================\n";
-							
-							
-							
-							if (!inside){
-								
-							}
-							
-						}
-					}	
-				
-					
+						inside = inside_walkable_area(
+							this->model->collider_mesh.value(),
+							mdl.walkable_area.value(),
+							this->position + velocity
+						);
+
+					}
 				}
-				
-				
+
 			} else {
 				velocity = glm::vec3(0.0f);
 			}
-		    position += velocity;
+			if (inside) {
+				this->position += velocity;
+			} 
 
+		}
+
+		bool inside_walkable_area(const Mesh &character_collider, const Mesh &walkable_area, const glm::vec3 position_to_test)
+		{
+						
+			//1. get character's 4 lowest pts of collider
+
+			std::array<glm::vec2, 4> small_square;
+
+			//there are 4*6 small_verts (4 per side, 6 sides of a cube)
+			const std::vector<Vertex> &small_verts = character_collider.vertices;
+			for (size_t i = 0; i < small_verts.size(); i+=4)
+			{
+				Vertex copy = small_verts[i];
+
+				// get the edge with normal = (0, -1, 0)
+				//a == b when `std::fabsf(a - b) < 1e-6`
+				bool copy_normal_is_minus_one = std::fabsf(copy.Normal.y - (-1.0f)) < 1e-6;
+				if (copy_normal_is_minus_one){
+					glm::vec3 p0 = small_verts[i+0].Position + position_to_test;
+					glm::vec3 p1 = small_verts[i+1].Position + position_to_test;
+					glm::vec3 p2 = small_verts[i+2].Position + position_to_test;
+					glm::vec3 p3 = small_verts[i+3].Position + position_to_test;
+
+					small_square = {
+						glm::vec2(p0.x, p0.z),
+						glm::vec2(p1.x, p1.z),
+						glm::vec2(p2.x, p2.z),
+						glm::vec2(p3.x, p3.z),
+					};
+
+					break;
+				}
+			}
+
+
+			//2. get all walkable areas triangles and their points
+
+			std::vector<std::array<glm::vec2, 3>> walkable_area_tris;
+			
+			const std::vector<Vertex> 		&walkable_verts 	= walkable_area.vertices;
+			const std::vector<unsigned int> &walkable_indeces 	= walkable_area.indices;
+
+			//0 1 2, 0 2 3
+			for (size_t i = 0; i < walkable_indeces.size(); i+=3)
+			{
+				glm::vec3 p0 = walkable_verts[walkable_indeces[i+0]].Position;
+				glm::vec3 p1 = walkable_verts[walkable_indeces[i+1]].Position;
+				glm::vec3 p2 = walkable_verts[walkable_indeces[i+2]].Position;
+
+				std::array<glm::vec2, 3> tri = {glm::vec2(p0.x, p0.z), glm::vec2(p1.x, p1.z), glm::vec2(p2.x, p2.z)};
+				walkable_area_tris.push_back(tri);
+			}
+
+
+			//3. check the intersection with walkable area
+
+			bool inside = Geometry2d::rect_inside_area_of_tris(
+				small_square,
+				walkable_area_tris
+			);
+
+			// std::cout << "small poly: \n";
+			// for (auto &pt_d : small_square)
+			// {
+			// 	std::cout << "\t{" << pt_d << "},";
+			// }
+			// std::cout << "\n";
+
+			// std::cout << "walkable_pts:\n";
+			//...
+			// std::cout << "inside area: " << inside << "\n";
+			// std::cout << "========================\n";
+
+			return inside;
 		}
 
 		void Update(const float dt) {
